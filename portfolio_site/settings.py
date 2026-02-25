3ea2d6de4e2332acd6a4d994d4b8dadc39f6a722
jayadeep <komarthijayadeep@gmail.com>
2026-02-25 14:49:13 +0530

    security-updates

--- portfolio_site/settings.py
+++ portfolio_site/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-tjy-t6r=t(ml3nux1y(p19xfk#r1dmr=dc$=hx-*hwbsxt8k&j'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
